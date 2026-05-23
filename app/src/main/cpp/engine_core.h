5c2a7336b87e9df452a77d8844f5599d96d2de31
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-05-22 20:46:12 -0600

    Create app/src/main/cpp/engine_core.h

---
+++ app/src/main/cpp/engine_core.h
@@ -0,0 +1,11 @@
+#ifndef ENGINE_CORE_H
+#define ENGINE_CORE_H
+
+namespace LostDungeons {
+    class Engine {
+    public:
+        static void runSimulation(long start, long end);
+    };
+}
+
+#endif
