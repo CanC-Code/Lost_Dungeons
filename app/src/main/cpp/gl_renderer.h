a4051fd2f18837cd8e0c6a8cc5456d66348a7f47
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-05-23 22:32:48 -0600

    Update gl_renderer.h

--- app/src/main/cpp/gl_renderer.h
+++ app/src/main/cpp/gl_renderer.h
@@ -3,29 +3,47 @@
 
 #include <android/native_window.h>
 #include <EGL/egl.h>
-#include <EGL/eglext.h> // ADDED: Required for EGL_OPENGL_ES3_BIT_KHR
+#include <EGL/eglext.h>
 #include <GLES3/gl3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <thread>
 #include <atomic>
+#include <mutex>
+#include <string>
 
 namespace LostDungeons {
+    enum class RenderState { OVERWORLD = 0, BATTLE = 1 };
+
     class GLRenderer {
     public:
         static void start(ANativeWindow* window);
         static void stop();
         static void setViewport(int width, int height);
+        
+        // NEW: Game State and Camera Controls
+        static void setGameState(int state, const std::string& entityId);
+        static void moveCamera(float dx, float dz);
 
     private:
         static std::atomic<bool> isRendering;
         static std::thread renderThread;
+        static std::mutex stateMutex;
         
         static EGLDisplay display;
         static EGLSurface surface;
         static EGLContext context;
         static GLuint shaderProgram;
+        static GLint mvpLocation;
 
         static int width;
         static int height;
+
+        // Camera & State variables
+        static glm::vec3 cameraPos;
+        static RenderState currentState;
+        static std::string activeEntity;
 
         static void renderLoop(ANativeWindow* window);
         static bool initEGL(ANativeWindow* window);
@@ -33,7 +51,10 @@ namespace LostDungeons {
         static GLuint loadShader(GLenum type, const char* shaderSrc);
         static void setupGraphics();
         static void drawFrame();
+        
+        // Geometry Drawing Helpers
+        static void drawOverworldFloor();
+        static void drawEntityCube();
     };
 }
-
 #endif
